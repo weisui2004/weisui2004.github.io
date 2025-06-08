b86fcd346fa6e928abd735a260ccdb848cd3645e
weisui2004 <weisui2023@163.com>
2025-06-09 01:31:16 +0800

    Site updated: 2025-06-09 01:31:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/24/Java基础知识/","2025/06/09/Java面向对象基础二/"];function toRandomPost(){
+var posts=["2025/06/09/Java面向对象基础二/","2025/04/24/Java基础知识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
